b230ab50fb5bff5d66db6376de40463895b2b48f
Franklin Ikeh <ikehfranklin06@gmail.com>
2024-07-06 02:14:53 +0100

    base project state

--- api/views.py
+++ api/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.request import Request
+from rest_framework.response import Response
 
-# Create your views here.
+@api_view()
+def pilot(request: Request):
+    return Response({"status": "LFGGGGGG"})
