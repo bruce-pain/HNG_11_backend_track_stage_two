9f9de20076b98433241dab7ef47e4f63568f021f
Franklin Ikeh <ikehfranklin06@gmail.com>
2024-07-08 15:12:21 +0100

    remove trailing slash

--- api/urls.py
+++ api/urls.py
@@ -4,7 +4,7 @@ from api import views
 
 urlpatterns = [
     path("users/<uuid:userId>", views.UserDetailAPIView.as_view(), name="user-detail"),
-    path("organisations/", views.OrganisationAPIView.as_view(), name="organisations"),
+    path("organisations", views.OrganisationAPIView.as_view(), name="organisations"),
     path(
         "organisations/<uuid:orgId>",
         views.OrganisationDetailAPIView.as_view(),
--- api_auth/urls.py
+++ api_auth/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from api_auth import views
 
 urlpatterns = [
-    path("register/", views.UserRegisterAPIView.as_view(), name="register"),
-    path("login/", views.UserLoginAPIView.as_view(), name="login"),
+    path("register", views.UserRegisterAPIView.as_view(), name="register"),
+    path("login", views.UserLoginAPIView.as_view(), name="login"),
 ]
