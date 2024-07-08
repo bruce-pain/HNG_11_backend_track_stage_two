4dc1d078d6d180baaafbda10be619a7db10823cd
Franklin Ikeh <ikehfranklin06@gmail.com>
2024-07-08 19:32:23 +0100

    diasble the browsable API

--- backend/settings.py
+++ backend/settings.py
@@ -39,6 +39,7 @@ REST_FRAMEWORK = {
         "rest_framework_simplejwt.authentication.JWTAuthentication",
     ),
     "DEFAULT_PERMISSION_CLASSES": ["rest_framework.permissions.IsAuthenticated"],
+    "DEFAULT_RENDERER_CLASSES": ("rest_framework.renderers.JSONRenderer",),
 }
 
 SIMPLE_JWT = {
